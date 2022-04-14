d9efdd02a4ba8fa5457c7e2361f97dafcc4bbf68
Kunal Kumar <38478306+kunalkumar168@users.noreply.github.com>
2022-04-15 01:23:58 +0530

    Update submitWithConfiguration.m

--- Assignment1/Excercise1/lib/submitWithConfiguration.m
+++ Assignment1/Excercise1/lib/submitWithConfiguration.m
@@ -1,7 +1,7 @@
 function submitWithConfiguration(conf)
   addpath('./lib/jsonlab');
 
-  parts = parts(conf);
+  parts = Parts(conf); % Updated
 
   fprintf('== Submitting solutions | %s...\n', conf.itemName);
 
@@ -22,21 +22,20 @@ function submitWithConfiguration(conf)
     response = submitParts(conf, email, token, parts);
   catch
     e = lasterror();
-    fprintf('\n!! Submission failed: %s\n', e.message);
-    fprintf('\n\nFunction: %s\nFileName: %s\nLineNumber: %d\n', ...
-      e.stack(1,1).name, e.stack(1,1).file, e.stack(1,1).line);
-    fprintf('\nPlease correct your code and resubmit.\n');
+    fprintf( ...
+      '!! Submission failed: unexpected error: %s\n', ...
+      e.message);
+    fprintf('!! Please try again later.\n');
     return
   end
 
   if isfield(response, 'errorMessage')
     fprintf('!! Submission failed: %s\n', response.errorMessage);
-  elseif isfield(response, 'errorCode')
-    fprintf('!! Submission failed: %s\n', response.message);
   else
     showFeedback(parts, response);
     save(tokenFile, 'email', 'token');
   end
+  rmpath('./lib/jsonlab', './lib'); % Updated
 end
 
 function [email token] = promptToken(email, existingToken, tokenFile)
@@ -63,15 +62,37 @@ end
 
 function response = submitParts(conf, email, token, parts)
   body = makePostBody(conf, email, token, parts);
-  submissionUrl = submissionUrl();
-
+  submissionUrl = SubmissionUrl(); % Updated
   responseBody = getResponse(submissionUrl, body);
-  jsonResponse = validateResponse(responseBody);
-  response = loadjson(jsonResponse);
+  response = loadjson(responseBody);
+end
+
+function response = getResponse(url, body)
+  % NEW CURL SUBMISSION FOR WINDOWS AND MAC
+  if ispc
+    new_body = regexprep (body, '\"', '\\"'); % will escape double quoted objects to format properly for windows libcurl
+    json_command = sprintf('curl -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/json" -d "%s" --ssl-no-revoke "%s"', new_body, url);
+    [code, response] = dos(json_command); %dos is for windows
+
+    new_response = regexp(response, '\{(.)*', 'match');
+    response = new_response{1,1};
+
+    % test the success code
+    if (code ~= 0)
+      fprintf('[error] submission with Invoke-WebRequest() was not successful\n');
+    end
+  else
+    json_command = sprintf('curl -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/json" -d '' %s '' --ssl-no-revoke ''%s''', body, url);
+    [code, response] = system(json_command);
+    % test the success code
+    if (code ~= 0)
+      fprintf('[error] submission with curl() was not successful\n');
+    end
+  end
 end
 
 function body = makePostBody(conf, email, token, parts)
-  bodyStruct.assignmentSlug = conf.assignmentSlug;
+  bodyStruct.assignmentKey = conf.assignmentKey;
   bodyStruct.submitterEmail = email;
   bodyStruct.secret = token;
   bodyStruct.parts = makePartsStruct(conf, parts);
@@ -89,7 +110,7 @@ function partsStruct = makePartsStruct(conf, parts)
   end
 end
 
-function [parts] = parts(conf)
+function [parts] = Parts(conf) % Updated
   parts = {};
   for partArray = conf.partArrays
     part.id = partArray{:}{1};
@@ -106,74 +127,24 @@ function showFeedback(parts, response)
   for part = parts
     score = '';
     partFeedback = '';
-    partFeedback = response.partFeedbacks.(makeValidFieldName(part{:}.id));
-    partEvaluation = response.partEvaluations.(makeValidFieldName(part{:}.id));
+    % NEW PARSING REPONSE BODY
+    partFeedback = response.linked.onDemandProgrammingScriptEvaluations_0x2E_v1{1}(1).parts.(makeValidFieldName(part{:}.id)).feedback;
+    partEvaluation = response.linked.onDemandProgrammingScriptEvaluations_0x2E_v1{1}(1).parts.(makeValidFieldName(part{:}.id));
     score = sprintf('%d / %3d', partEvaluation.score, partEvaluation.maxScore);
     fprintf('== %43s | %9s | %-s\n', part{:}.name, score, partFeedback);
   end
-  evaluation = response.evaluation;
+  evaluation = response.linked.onDemandProgrammingScriptEvaluations_0x2E_v1{1}(1);
   totalScore = sprintf('%d / %d', evaluation.score, evaluation.maxScore);
   fprintf('==                                   --------------------------------\n');
   fprintf('== %43s | %9s | %-s\n', '', totalScore, '');
   fprintf('== \n');
 end
 
-% use urlread or curl to send submit results to the grader and get a response
-function response = getResponse(url, body)
-% try using urlread() and a secure connection
-  params = {'jsonBody', body};
-  [response, success] = urlread(url, 'post', params);
-
-  if (success == 0)
-    % urlread didn't work, try curl & the peer certificate patch
-    if ispc
-      % testing note: use 'jsonBody =' for a test case
-      json_command = sprintf('echo jsonBody=%s | curl -k -X POST -d @- %s', body, url);
-    else
-      % it's linux/OS X, so use the other form
-      json_command = sprintf('echo ''jsonBody=%s'' | curl -k -X POST -d @- %s', body, url);
-    end
-    % get the response body for the peer certificate patch method
-    [code, response] = system(json_command);
-    % test the success code
-    if (code ~= 0)
-      fprintf('[error] submission with curl() was not successful\n');
-    end
-  end
-end
-
-% validate the grader's response
-function response = validateResponse(resp)
-  % test if the response is json or an HTML page
-  isJson = length(resp) > 0 && resp(1) == '{';
-  isHtml = findstr(lower(resp), '<html');
-
-  if (isJson)
-    response = resp;
-  elseif (isHtml)
-    % the response is html, so it's probably an error message
-    printHTMLContents(resp);
-    error('Grader response is an HTML message');
-  else
-    error('Grader sent no response');
-  end
-end
-
-% parse a HTML response and print it's contents
-function printHTMLContents(response)
-  strippedResponse = regexprep(response, '<[^>]+>', ' ');
-  strippedResponse = regexprep(strippedResponse, '[\t ]+', ' ');
-  fprintf(strippedResponse);
-end
-
-
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % Service configuration
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function submissionUrl = submissionUrl()
-  submissionUrl = 'https://www-origin.coursera.org/api/onDemandProgrammingImmediateFormSubmissions.v1';
+function submissionUrl = SubmissionUrl() % Updated
+  submissionUrl = 'https://www.coursera.org/api/onDemandProgrammingScriptSubmissions.v1?includes=evaluation';
 end
